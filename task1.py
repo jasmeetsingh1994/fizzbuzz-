f1366dbb860b120eac459491c16b36a0d35bc99d
sujanrp <sj213151@dal.ca>
2019-05-23 06:06:03 -0700

    adding three books

--- task1.py
+++ task1.py
@@ -10,3 +10,15 @@ target=input("textbook1 textbook2 textbook3")
 	elif target = "textbook3":
 		fr=open('textbook3.txt','r')
 	else target = input("enter a valid book again")		
+
+def enterpage():
+	fw = open('withnewfile.txt','w')
+	count=0
+	pageswillbe = input("please enter your first page number into it")
+	pageswillbe = input("and also enter your lastr page number ")
+	pageswillbe = input("again first page and then enter your last page ")
+	for a in range(0,(25*pages)):
+		count=1
+		if count>=(25*pages):
+                	fw.write(	
+					
