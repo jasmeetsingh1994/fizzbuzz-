2cf0346e0b344bba4d214a1d4ae2cc425c9dbe81
sujanrp <sj213151@dal.ca>
2019-05-23 05:31:22 -0700

    assigning 3 text books and checking whether it is correct book or not

--- task1.py
+++ task1.py
@@ -1,3 +1,11 @@
+
+
 target=input("textbook1 textbook2 textbook3")
-if target = "textbook1":
-	
+	if target = "textbook1":
+		fr=open('textbook1.txt','r')
+		check1=1
+	elif target = "textbook2":
+		fr=open('textbook2.txt','r')
+	elif target = "textbook3":
+		fr=open('textbook3.txt','r')
+		
